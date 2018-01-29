70d160e0f80eeac26c3249283357506f3efa0a09
AbhilashJN <abhilashjn107@gmail.com>
2018-01-29 21:56:52 +0530

    fulfill.js : added test case to check against different string and check return type

--- fulfill.test.js
+++ fulfill.test.js
@@ -3,6 +3,19 @@ let promise = require("./fulfill");
 
 test("Should print FULFILLED!", ()=>{
 
-	return expect(promise).resolves.toBe("FULFILLED!");
+ 	return expect(promise).resolves.toBe("FULFILLED!");
+    
+});
+
+test("Should not print asd", ()=>{
+
+	return expect(promise).resolves.not.toBe("asd!");
+    
+});
+
+
+test("Should return string type", ()=>{
+
+	return expect(promise).resolves.toEqual(expect.any(String));
     
 });
